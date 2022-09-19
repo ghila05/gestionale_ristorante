7ddc90a03fe51d9ad58a572369e1dfa6b6f3f18d
ghila05 <nicolaghi05@gmail.com>
2022-09-19 16:58:21 +0200

    Versione finale

--- gestionale_ristorante/Form1.Designer.cs
+++ gestionale_ristorante/Form1.Designer.cs
@@ -101,6 +101,7 @@ namespace gestionale_ristorante
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(201, 22);
             this.textBox1.TabIndex = 4;
+            this.textBox1.UseSystemPasswordChar = true;
             this.textBox1.Visible = false;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
